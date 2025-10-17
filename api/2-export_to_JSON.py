d8a3687b20dd8f7da9166b12a47ab865502f4d00
bintwaza <b.intwaza@alustudent.com>
2025-10-17 15:40:37 +0200

    basic

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,81 +1,36 @@
-
 #!/usr/bin/python3
-"""
-2-export_to_JSON.py
+"""Script that gets user data (Todo list) from API
+and then export the result to csv file. """
 
-This moodule exports data to JSON file from an APIendpoint:
-URL: https://jsonplaceholder.typicode.com
-
-Libraries used:
-            - sys
-            - reqeusts
-            - and more
-
-Author: Aimable
-Date: October 2024
-"""
 import json
 import requests
 import sys
 
 
-def export_data(id):
-    """
-    This function fetches and exports in json file from an API endpoint
-
-    Args:
-        id (str): the id that identifies the user whose data we are retrieving
-
-    Raises:
-        -reqeusts.ReqeustException: if there's an errro making the request
-        -Exception: for other general exceptions
-
-    Returns:
-        No return.
-    """
-    try:
-        url1 = "https://jsonplaceholder.typicode.com/users"
-        params = {"id": id}
-        response = requests.get(url1, params=params)
-        user_data = response.json()
-        USER_ID = str(user_data[0]["id"])
-        USERNAME = user_data[0]["username"]
-
-        url2 = "https://jsonplaceholder.typicode.com/todos"
-        params_1 = {"userId": id}
-        response = requests.get(url2, params=params_1)
-        todos = response.json()
-        sample_list = list()
-        sample_dict = dict()
-        for item in todos:
-            sample_dict = {
-                "task": item["title"],
-                "completed": item["completed"],
-                "username": USERNAME,
-            }
-            sample_list.append(sample_dict)
-        final_dict = {USER_ID: sample_list}
-        with open(f"{USER_ID}.json", "w") as file:
-            json.dump(final_dict, file, indent=2)
-
-    except requests.RequestException as error:
-        print(f"Failed to retrieve data. \nError message:{error}")
-    except Exception as error:
-        print(f"Request Failed, error: {error}")
-
-
 def main():
-    """
-    Main function that checks if an argument is passed at the command-line
-    and fetches the data and saves it into a json file for the given user ID
-    """
-    if len(sys.argv) > 1:
-        id = sys.argv[1]
-        export_data(id)
-    else:
-        print("Please provide an id")
-        sys.exit(1)
+    """main function"""
+    user_id = int(sys.argv[1])
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+
+    response = requests.get(todo_url)
+    user_name = requests.get(user_url).json().get('username')
+    user_data = []
+    output = {user_id: user_data}
+
+    for todo in response.json():
+        if todo.get('userId') == user_id:
+            user_data.append(
+                {
+                    "task": todo.get('title'),
+                    "completed": todo.get('completed'),
+                    "username": user_name,
+                })
+    print(output)
+    file_name = "{}.json".format(user_id)
+    with open(file_name, 'w') as file:
+        json.dump(output, file)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,76 +1,37 @@
-
 #!/usr/bin/python3
-"""
-2-export_to_JSON.py
+"""Script that gets user data (Todo list) from API
+and then export the result to csv file. """
 
-This moodule exports data in the JSON format from an API endpoint:
-URL: https://jsonplaceholder.typicode.com
-
-Libraries used:
-            - reqeusts
-            - json
-
-Author: Aimable
-Date: October 2024
-"""
 import json
 import requests
 
 
-def export_to_json():
-    """
-    This function fetches and saves data in Json format from an API endpoint
-
-    Args:
-        id (str): the id that identifies the user whose data we are retrieving
-
-    Raises:
-        -reqeusts.ReqeustException: if there's an errro making the request
-        -Exception: for other general exceptions
-
-    Returns:
-        No return.
-    """
-    try:
-        url1 = "https://jsonplaceholder.typicode.com/users"
-        url2 = "https://jsonplaceholder.typicode.com/todos"
-        user_data = requests.get(url1)
-        todos = requests.get(url2)
-        user_data = user_data.json()
-        todos = todos.json()
-
-        # USERNAME = user_data[0]["username"]
-        init_dict = dict()
-        temp_list = list()
-        temp_dict = dict()
-        i = 0
-        for id in user_data:
-            USER_ID = user_data[i]["id"]
-            if USER_ID not in init_dict.keys():
-                for item in todos:
-                    if item["userId"] == USER_ID:
-                        temp_dict = {
-                            "username": user_data[i]["username"],
-                            "task": item["title"],
-                            "completed": item["completed"],
-                        }
-                        temp_list.append(temp_dict)
-                init_dict[f"{USER_ID}"] = temp_list
-                temp_list = []
-            i += 1
-        with open("todo_all_employees.json", "w") as file:
-            json.dump(init_dict, file, indent=2)
-
-    except Exception as error:
-        print(f"Oops!, Error encountered: {error}")
-
-
 def main():
-    """
-    fetches the data and saves it into a json file
-    """
-    export_to_json()
+    """main function"""
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    response = requests.get(todo_url)
+
+    output = {}
+
+    for todo in response.json():
+        user_id = todo.get('userId')
+        if user_id not in output.keys():
+            output[user_id] = []
+            user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+                user_id)
+            user_name = requests.get(user_url).json().get('username')
+
+        output[user_id].append(
+            {
+                "username": user_name,
+                "task": todo.get('title'),
+                "completed": todo.get('completed')
+            })
+
+    with open("todo_all_employees.json", 'w') as file:
+        json.dump(output, file)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
